a3ca5acd01e6e7dd377859b7b6446f2a226d4f33
nilesh_indore <nilesh_indore@II24-13TJPH2.mhf.mhc>
2020-02-20 21:09:13 +0530

    adding files...

---
+++ env.bat
@@ -0,0 +1,3 @@
+set JAVA_HOME=C:\Nilesh Indore\APPS\jdk
+set MAVEN_HOME=C:\Nilesh Indore\APPS\maven
+set PATH=%JAVA_HOME%/bin;%MAVEN_HOME%/bin;%PATH%
---
+++ env.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/local/apps/software/jdk
+export MAVEN_HOME=/local/apps/software/maven
+
+PATH=$JAVA_HOME/bin;$MAVEN_HOME/bin;$PATH
+
